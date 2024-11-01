63fabc71ad772a9c506e90e60aa1c366990718a8
zuedev <zuedev@gmail.com>
2024-11-01 01:03:45 +0000

    initial commit

---
+++ addons/modern_ops/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+    class modern_ops {
+        authors[] = {"zuedev"};
+        authorUrl = "https://uagpmc.com";
+        name = "modern_ops";
+        requiredAddons[] = {};
+        requiredVersion = 2.16;
+        units[] = {};
+        weapons[] = {};
+    };
+};
